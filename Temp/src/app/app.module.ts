39707a12006f63b72a0f0878948ba4f88372e818
Jagadeesh Bandlamudi <9443987+jags1891@users.noreply.github.com>
2019-01-06 20:05:09 -0800

    Add files via upload

---
+++ Temp/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ArchiveComponent } from './archive/archive.component';
+
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ArchiveComponent,
+    HomeComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(
+      [
+        {path:'', component:HomeComponent},
+        {path:'archive/:year/:month' , component: ArchiveComponent},
+        {path:'**', component:NotFoundComponent}
+      ]
+    )
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
