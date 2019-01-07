95c0a3e5b1f29c8396559b1615bf2d457fbe32c2
Jagadeesh Bandlamudi <9443987+jags1891@users.noreply.github.com>
2019-01-06 19:34:20 -0800

    Add files via upload

---
+++ complex-domains/before/redux-demo/src/app/tasking/tasking.actions.ts
@@ -0,0 +1,5 @@
+export const ADD_TODO = 'ADD_TODO';
+export const TOGGLE_TODO = 'TOGGLE_TODO';
+export const REMOVE_TODO = 'REMOVE_TODO';
+// tslint:disable-next-line:eofline
+export const CLEAR_TODOS = 'CLEAR_TODOS';
